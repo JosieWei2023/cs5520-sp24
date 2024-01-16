25c872d197895d4ba8cf08ecc70eb8a0ea9a6c59
Josie <tinaweizq@outlook.com>
2024-01-16 14:56:05 -0800

    Add TextInput and Text components to App.js

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
   const appName = "My First React Native App";
+  const [text, setText] = useState("Type here!");
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header appName={appName} />
+      <TextInput style={styles.textInput} value={text} onChangeText={setText} />
+      <Text style={styles.text}>{text}</Text>
     </View>
   );
 }
@@ -18,5 +23,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    borderBottomColor: "purple",
+    borderBottomWidth: 2,
+    width: "50%",
+  },
+  text: {
+    backgroundColor: "yellow",
+    borderColor: "black",
+    width: "50%",
+    height: "30%",
   },
 });
