7e480995ffaf6d637c837a82daaa93156e99204b
Josie <tinaweizq@outlook.com>
2024-01-16 14:02:05 -0800

    Add Header component to App.js

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
   const appName = "My First React Native App";
   return (
     <View style={styles.container}>
-      <Text>Wlecome to {appName}!</Text>
       <StatusBar style="auto" />
+      <Header appName={appName} />
     </View>
   );
 }
