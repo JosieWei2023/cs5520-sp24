c20e06e63440eafec04fee4b1d5110d57de4fa87
Josie <tinaweizq@outlook.com>
2024-01-16 13:45:15 -0800

    add a variable for app name and use it in the welcome message

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const appName = "My First React Native App";
   return (
     <View style={styles.container}>
-      <Text>hhhhhhhhhhhh!</Text>
+      <Text>Wlecome to {appName}!</Text>
       <StatusBar style="auto" />
     </View>
   );
